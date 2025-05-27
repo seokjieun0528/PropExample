1c479de4aa543ede2f8d1581de1c959d6c944577
Another Name <s2307@e-mirim.hs.kr>
2025-05-27 14:50:29 +0900

    Box 클래스 getter, setter 구현

--- Box.cs
+++ Box.cs
@@ -11,6 +11,8 @@ namespace PropExample
         private int width;
         private int height;
 
+
+        // 고전적인 스타일
         public Box(int w, int h)
         {
             if(width > 0 && height > 0)
@@ -24,6 +26,23 @@ namespace PropExample
                 //throw new Exception("너비와 높이는 자연스로 초기화해주세요");
             }
         }   // Box
+
+
+        // 자바
+        public int getWidth() { return this.width; }
+        public int getHeight() { return this.height; }
+
+        public void setWidth(int width)
+        {
+            if(width>0) { this.width = width; }
+            else { Console.WriteLine("너비는 자연수로 입력해주세요"); }
+        }
+
+        public void setHeight(int height)
+        {
+            if (width > 0) { this.height = height; }
+            else { Console.WriteLine("높이는 자연수로 입력해주세요"); }
+        }
 
         public int Area()
         {
