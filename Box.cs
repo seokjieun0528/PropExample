c481158cae70adb6b9f5757c475838107caef98d
Another Name <s2307@e-mirim.hs.kr>
2025-05-27 14:45:54 +0900

    고전적인 스타일로  Box 클래스 정의

---
+++ Box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropExample
+{
+    class Box
+    {
+        private int width;
+        private int height;
+
+        public Box(int w, int h)
+        {
+            if(width > 0 && height > 0)
+            {
+                this.width = w;
+                this.height = h;
+            }
+            else
+            {
+                Console.WriteLine("너비와 높이는 자연스로 초기화해주세요");
+                //throw new Exception("너비와 높이는 자연스로 초기화해주세요");
+            }
+        }   // Box
+
+        public int Area()
+        {
+            return this.width = this.height;
+        }
+
+    } // Class Box
+}
