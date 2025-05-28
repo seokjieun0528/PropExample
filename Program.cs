c83ad81d957ac1af68b5d87cf25055e9de462b35
Another Name <s2307@e-mirim.hs.kr>
2025-05-28 13:55:57 +0900

    클래스 변수 호출 주의

--- Program.cs
+++ Program.cs
@@ -8,8 +8,16 @@ namespace PropExample
 {
     class Program
     {
+        public static int classVar = 0;
         static void Main(string[] args)
         {
+            // 클래스 변수 호출
+            Program.classVar = -1;
+
+            // 인스턴스 변수처럼 클래스 변수 사용 불가에 주의(자바에서는 가능 해깔리지 않도록 주의)
+            //Program p = new Program();
+            //p.classVar = -3
+
             Box box1 = new Box(100, 200);
             box1.Width = -3;
             Console.WriteLine(box1.Width);
